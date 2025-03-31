a713fb425a23a0de9f770cce2d59392219adcd8f
nish_hack_06 <83601466+NSmonish@users.noreply.github.com>
2025-03-31 19:35:22 +0530

    Update App.jsx
    Added Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,56 @@
-import { useState } from 'react';
-import SignUp from './SignUp.jsx';
-import Login from './Login.jsx';
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Login from "./pages/Login";
+import Signup from "./pages/SignUp";
+import AdminPanel from "./pages/AdminPanel";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Unauthorized from "./pages/Unauthorized";
+import Intro from "./pages/Intro";
+import ToDream from "./pages/ToDream";
+import Features from "./pages/Features";
+import ContactUs from "./pages/ContactUs";
 
-function App() {
+const Home = () => {
   return (
-    <SignUp />
+    <>
+      <Intro />
+      <ToDream />
+      <Features />
+      <ContactUs />
+    </>
   );
-}
+};
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Home />} />
+        {/*<Route
+          path="/"
+          element={
+            <ProtectedRoute allowedRoles={["user", "admin"]}>
+              <Home />
+            </ProtectedRoute>
+          }
+        />*/}
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute allowedRoles={["admin"]}>
+              <AdminPanel />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/unauthorized" element={<Unauthorized />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+};
 
 export default App;
