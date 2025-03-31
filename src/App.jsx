b2a4697c298791bc97c45d3c299b5209bb7ecd76
nish_hack_06 <83601466+NSmonish@users.noreply.github.com>
2025-03-31 08:05:29 +0530

    Login and Signup Pages

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import SignUp from './SignUp.jsx';
+import Login from './Login.jsx';
+
+function App() {
+  return (
+    <SignUp />
+  );
+}
+
+export default App;
